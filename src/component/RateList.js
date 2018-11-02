d02410d5883a8e30d653e9bdc9120754f4b16caf
sika20 <sidra.kazmi@comhem.com>
2018-11-02 11:52:48 +0100

    added list

--- src/component/RateList.js
+++ src/component/RateList.js
@@ -3,47 +3,57 @@ import React, { Component } from 'react';
 
 class RateList extends Component {
 
-  state = {
-    currencies: [],
-  };
+    state = {
 
-  getRateList = () => { 
-      fetch('https://api.exchangeratesapi.io/latest')
-        .then((response) => {
-          return response.json();
-        }).then((myJson) => {
-            resolve(myJson.rates);       
+        currencies: [],
+    };
+
+    componentDidMount() {
+        this.getRateList();
+    }
+
+    getRateList = () => {
+
+        fetch('https://api.exchangeratesapi.io/latest')
+            .then((response) => {
+                return response.json();
+            }).then((myJson) => {
                 console.log(myJson.rates);
 
-          let currencyAr = []
-             for (const key in myJson.rates) {
-               currencyAr.push(key)
-             }
- 
-             this.setState({ currencies: currencyAr })
+                let currencyAr = []
+                for (const key in myJson.rates) {
+                    console.log(key, myJson.rates[key]);
+                    currencyAr.push({ key, value: myJson.rates[key]})
+                }
 
-        })
-  }
-/*
-  changeColor = (e) => {
-    this.setState({ toCurrency: e.target.value }, () => {
-    });
-  }
-*/
+                this.setState({ currencies: currencyAr })
 
+            })
+    }
+    /*
+      changeColor = (e) => {
+        this.setState({ toCurrency: e.target.value }, () => {
+        });
+      }
+    */
 
-  render() {
-    return (
-      <div className="list">
-        <h1>Currency Rate List </h1>
+    render() {
+        return (
+        <div className="list">
+        <h2>Currency Rate List </h2>
 
-          <button className="btn" onClick={this.getRateList} > Click me! </button>
-          <h2>{this.state.currencies}</h2>
+         <ul> 
+             Rate of Euro to
+            {this.state.currencies.map(currency => (
+                        <li key={currency.key}>
+                            {currency.key} : {currency.value} ;
+                         </li>
+                    ))}
+                </ul>
+        </div>
 
-        </div>    
-      
-    );
-  }
+        );
+    }
 }
 
 export default RateList;
